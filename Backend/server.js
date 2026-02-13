93a066baa21d55174cb8db84bcbd18378f4843d5
Ritika Mittal <ritikamittal@MacBook-Air-5.local>
2026-02-14 00:01:06 +0530

    railway binding

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,6 @@ const server=http.createServer(app);
 
 initializeSocket(server);
 
-server.listen(port,()=>{
+server.listen(port,'0.0.0.0',()=>{
     console.log(`Server is running on port ${port}`);
 })
